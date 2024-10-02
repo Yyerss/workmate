84418abdeedd4b44cdac5a22464f0b132a8ee17f
Yersultan Zhubandyk <dg078@digis.lan>
2024-10-02 14:41:13 +0500

    starting auth

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+# Custom apps
+INSTALLED_APPS += [
+    'apps.custom_auth.CustomAuthConfig'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
